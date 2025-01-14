04c0942eef5d8143f2b6f83dccb29f64daaa70a7
최은석 <trgf456@naver.com>
2025-01-14 19:44:47 +0900

    feat : 아이콘 컴포넌트 아이콘들추가

--- src/components/common/Icons/index.ts
+++ src/components/common/Icons/index.ts
@@ -3,6 +3,10 @@ import { default as DownTriangle } from './DownTriangle';
 import { default as UpTriangle } from './UpTriangle';
 import { default as Search } from './Search';
 import { default as People } from './People';
+import { default as Building } from './Building';
+import { default as Map } from './Map';
+import { default as Traffic } from './Traffic';
+import { default as Restaurant } from './Restaurant';
 
 export const Icons = {
   DownTriangle,
@@ -10,6 +14,10 @@ export const Icons = {
   Close,
   Search,
   People,
+  Building,
+  Map,
+  Traffic,
+  Restaurant,
 };
 
 export default Icons;
