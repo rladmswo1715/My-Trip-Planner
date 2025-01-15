2f1f5ddba2ddad41577570999d19a60788b4687b
최은석 <trgf456@naver.com>
2025-01-15 22:05:31 +0900

    chore : 리액트쿼리 devtool 추가

--- src/providers/RootProviders.tsx
+++ src/providers/RootProviders.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import QueryProviders from './QueryProviders';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const RootProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <QueryProviders>{children}</QueryProviders>
+      <QueryProviders>
+        {children}
+        <ReactQueryDevtools />
+      </QueryProviders>
     </>
   );
 };
