82fd87aa861eff4784b2272705ea8b53db856917
eunjae <rladmswo1715@naver.com>
2025-03-03 14:55:26 +0900

    fix: rating 컴포넌트 관련 이미지 설정

--- src/constants/importImages.ts
+++ src/constants/importImages.ts
@@ -22,6 +22,8 @@ import IconEdit from '@/assets/icon/ic_edit.svg';
 import IconDelete from '@/assets/icon/ic_delete.svg';
 import IconEye from '@/assets/icon/ic_eye.svg';
 import IconLock from '@/assets/icon/ic_lock.svg';
+import IconStarOn from '@/assets/icon/ic_star-on.svg';
+import IconStarOff from '@/assets/icon/ic_star-off.svg';
 // import IconClose from '@/assets/icon/close.svg';
 
 type ImageType = {
@@ -52,7 +54,9 @@ type Icons =
   | 'iconEdit'
   | 'iconDelete'
   | 'iconEye'
-  | 'iconLock';
+  | 'iconLock'
+  | 'iconStarOn'
+  | 'iconStarOff';
 // | 'iconClose';
 
 type Images = 'defaultProfile';
@@ -163,5 +167,13 @@ export const ICONS: ExportAssetsType<Icons> = {
   iconLock: {
     src: IconLock,
     alt: '잠금 아이콘',
+  },
+  iconStarOn: {
+    src: IconStarOn,
+    alt: '별점',
+  },
+  iconStarOff: {
+    src: IconStarOff,
+    alt: '별점',
   },
 };
