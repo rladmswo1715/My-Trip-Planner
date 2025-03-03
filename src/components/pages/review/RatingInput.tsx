93248d34303fa4fc13f38c7b0ce5a60416654dd4
eunjae <rladmswo1715@naver.com>
2025-03-03 14:54:30 +0900

    feat: rating 컴포넌트 추가

---
+++ src/components/pages/review/RatingInput.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { ICONS } from '@/constants/importImages';
+import Image from 'next/image';
+import { useReviewStore } from '@/stores/reviewStores';
+
+const { iconStarOn, iconStarOff } = ICONS;
+const RATINGS = [1, 2, 3, 4, 5];
+
+interface StarProps {
+  selected: boolean;
+  onClick: () => void;
+  onMouseOver: () => void;
+  onMouseOut: () => void;
+}
+
+function Star({ selected, onClick, onMouseOut, onMouseOver }: StarProps) {
+  const StarAction = selected ? iconStarOn.src : iconStarOff.src;
+
+  return (
+    <Image
+      width={40}
+      height={40}
+      src={StarAction}
+      alt={iconStarOn.alt}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+      onClick={onClick}
+      className="cursor-pointer"
+    />
+  );
+}
+
+interface RatingInputProps {
+  question: string;
+}
+
+export default function RatingInput({ question }: RatingInputProps) {
+  const [hoverRating, setHoverRating] = useState(0);
+  const { ratings, setRating } = useReviewStore();
+  const rating = ratings[question] || 0;
+
+  const handleStarClick = (value: number) => {
+    setRating(question, value);
+  };
+
+  const handleStarMouseOver = (value: number) => {
+    setHoverRating(value);
+  };
+
+  const handleStarMouseOut = () => {
+    setHoverRating(0);
+  };
+
+  return (
+    <div className="flex flex-col gap-[0.8]">
+      <p className="text-[1.6rem] leading-[2.08rem]">{question}</p>
+      <ul className="flex gap-[0.1rem]">
+        {RATINGS.map((starRating) => (
+          <li key={starRating}>
+            <Star
+              selected={starRating <= (hoverRating || rating)}
+              onClick={() => handleStarClick(starRating)}
+              onMouseOver={() => handleStarMouseOver(starRating)}
+              onMouseOut={handleStarMouseOut}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
